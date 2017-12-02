51dea8f61df5ac53a8dcf476a3afcaacc92c61a7
Jakob Jul Elben <elbenjakobjul@gmail.com>
2017-12-02 14:29:39 +0100

    Add lm implementation

---
+++ R/lm.R
@@ -0,0 +1,22 @@
+#' Ordinary Least Squares
+#'
+#' @param data A data.frame containing the dependent and independent variables used in the regression.
+#' @inheritParams stats::lm
+#' @param robust Logical. If true robust standard errors is used.
+#'
+#' @return mod_lm returns an object of the class mod_lm, which is a list containing information about the fitted model.
+#' @export
+#'
+#' @examples mod_lm(mtcars, mpg~cyl)
+mod_lm <- function(data, formula, robust = FALSE) {
+  mf <- model.frame(formula = formula, data = data)
+  X <- model.matrix(attr(mf, "terms"), data = mf)
+  y <- model.response(mf)
+  res <- lm_rcpp(X, y, robust)
+  res$call <- match.call()
+  res$coefficients <- as.vector(res$coefficients)
+  res$formula <- formula
+  names(res$coefficients) <- colnames(X)
+  class(res) <- "mod_lm"
+  res
+}
--- R/modelR.R
+++ R/modelR.R
@@ -1,3 +1,4 @@
 #' @useDynLib modelR, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
+#' @importFrom stats model.frame model.matrix model.response
 "_PACKAGE"
