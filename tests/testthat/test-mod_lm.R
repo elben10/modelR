b1fca38568adb55a5fb9e9ce01cbe440a5092b38
Jakob Jul Elben <elbenjakobjul@gmail.com>
2017-12-02 15:05:21 +0100

    Add tests

--- tests/testthat/test-mod_lm.R
+++ tests/testthat/test-mod_lm.R
@@ -1,5 +1,13 @@
 context("test-mod_lm.R")
 
+# Test mod_lm
 test_that("mod_lm returns list of class mod_lm", {
-  testthat::expect_s3_class(mod_lm(mtcars, mpg~cyl), "mod_lm")
+  expect_is(mod_lm(mtcars, mpg~cyl), "mod_lm")
+  expect_type(mod_lm(mtcars, mpg~cyl), "list")
+})
+
+# Test summary of mod_lm
+test_that("summary of mod_lm returns list of class summary.mod_lm", {
+  expect_is(summary(mod_lm(mtcars, mpg~cyl)), "summary.mod_lm")
+  expect_type(summary(mod_lm(mtcars, mpg~cyl)), "list")
 })
